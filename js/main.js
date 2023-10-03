06d0375dd067a7949ed2087bd2ce466e8a3cf165
arsennyrov <arsenyrov@mail.ru>
2023-10-03 16:17:53 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+
+  $('.slider-blog__inner').slick({
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false
+        }
+      },
+    ]
+  });
+
+  $('.menu__btn, .menu a').on('click', function () {
+    $('.header__top-inner').toggleClass('header__top-inner--active');
+  });
+
+  var mixer = mixitup('.portfolio__content');
+
+});
